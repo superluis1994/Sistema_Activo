af7989b2ccd0cedaaeac2d99993aa5a669e23322
superluis1994 <alberto.sorto20@itca.edu.sv>
2022-07-06 13:23:35 -0600

    avance21

--- js/list_activos/list_activo.js
+++ js/list_activos/list_activo.js
@@ -1,12 +1,12 @@
 document.getElementById("opt").addEventListener("click", function(e) {
-   document.getElementById("busqueda").setAttribute("hidden", true);
+   document.getElementById("busqueda").setAttribute("hidden", false);
    document.getElementById("busquedaPer").removeAttribute("hidden")
 
 })
 document.getElementById("op1").addEventListener("click", function(e) {
-    document.getElementById("busquedaPer").setAttribute("hidden", false);
-    document.getElementById("busqueda ").removeAttribute("hidden")
-    
+    // document.getElementById("busqueda").setAttribute("hidden", false);
+    document.getElementById("busqueda").removeAttribute("hidden")
+    document.getElementById("busquedaPer").setAttribute("hidden", true);
  
  })
  document.getElementById("op2").addEventListener("click", function(e) {
